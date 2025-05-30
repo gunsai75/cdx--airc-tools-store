7416c26883f918d09f0c0487b0a1485241913438
Sai Ganesha <saiiiigm1509@gmail.com>
2025-05-30 20:11:10 +0530

    NextJS upgrade and some slight cosmetic improvements

--- src/components/layout/home-page-header.tsx
+++ src/components/layout/home-page-header.tsx
@@ -1,15 +1,19 @@
 import { AuroraText } from "../magicui/aurora-text";
 import { WordRotate } from "../magicui/word-rotate";
-
+  
 export default function HomePageHeaderTitle() {
     return (
-        <h3 className="mt-10 w-full text-2xl text-center font-bold tracking-tighter lg:text-6xl">
-            Tools to make your life <AuroraText className="w-[100px] lg:w-[300px]">
-                <WordRotate
-
-                    words={["Easy", "Fun", "Awesome", "Amazing", "Great", "Good", "Cool", "Awesome", "Amazing", "Great", "Good", "Cool"]}
-                />
-            </AuroraText>
-        </h3>
+        <div className="mt-10 w-full text-center font-bold tracking-tighter lg:text-6xl">
+            <h3 className="mt-10 w-full text-2xl text-center font-bold tracking-tighter lg:text-6xl">
+                Tools to make your life
+            </h3>
+            <div style={{marginTop: '5px'}}> {/* Add margin top for spacing */}
+                <AuroraText className="w-[100px] lg:w-[300px]">
+                    <WordRotate
+                        words={["Easy", "Fun", "Awesome", "Amazing", "Great", "Good", "Cool"]}
+                    />
+                </AuroraText>
+            </div>
+        </div>
     )
 }
